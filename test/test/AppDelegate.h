5635662ebc48488729f09d42be2518a6c74017aa
王翔 <446192049@qq.com>
2016-07-29 11:18:41 +0800

    新建xcode工程

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 王翔 on 16/7/29.
+//  Copyright © 2016年 sam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
